3bcb123e63e3a5f0e969e4e9e5079ba47108f5ea
Juan24rangel <juanjorangel24@gmail.com>
2025-04-17 12:33:35 -0500

    Fase 5

--- modulo/urls.py
+++ modulo/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('login/', auth_views.LoginView.as_view(template_name='modulo/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='index'), name='logout'),
     path('videojuego/<int:videojuego_id>/', views.detalle_videojuego, name='detalle_videojuego'),
 ]
 
